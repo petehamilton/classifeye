254391bf553e220e2c76cbb6a8bcf7cf88569991
Peter Hamilton <peter@inspiredpixel.net>
2012-05-19 01:08:24 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openlabs::Application.config.secret_token = '5caab0d08a5874e166b818b1b10ad0a35f9bee5c324930ed34e13a78c3a2ddde40c8396cedf37622e556f571c6dbdc708c5f6e2c76d547410b72199e46fd6aed'
