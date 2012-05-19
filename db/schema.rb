fd9f20f874491252ce511efbdacd68171379a66f
Peter Hamilton <peter@inspiredpixel.net>
2012-05-19 14:24:57 +0100

    Fixed an issue with spot database entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120519110704) do
+ActiveRecord::Schema.define(:version => 20120519132407) do
 
   create_table "spots", :force => true do |t|
     t.string   "reference"
